1567805a38bd3af527848fc50ea6714d39a3557c
Lucas Moreira <lucasgbd@hotmail.com.br>
2020-01-26 15:24:15 +0000

    Real Estate Init

---
+++ RealEstate/App_Start/RealEstateContext.cs
@@ -0,0 +1,24 @@
+﻿namespace RealEstate.App_Start
+{
+	using MongoDB.Driver;
+	using Properties;
+
+	public class RealEstateContext
+	{
+		public MongoDatabase Database;
+
+		public RealEstateContext()
+		{
+			var client = new MongoClient(Settings.Default.RealEstateConnectionString);
+			var server = client.GetServer();
+			Database = server.GetDatabase(Settings.Default.RealEstateDatabaseName);
+		}
+	}
+}
+
+
+
+
+
+
+
